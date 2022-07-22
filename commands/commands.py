cfb2af4ca89711ac2930f0fa06e1130b3afb2783
Mark Baggett <mbagget1@utk.edu>
2022-07-22 10:21:17 -0500

    Add dummy get_bookings response.

--- commands/commands.py
+++ commands/commands.py
@@ -1,5 +1,4 @@
 import arrow
-from flask import request
 
 
 class GetResponse:
@@ -9,21 +8,22 @@ class GetResponse:
 
     def __get_command(self, request_body):
         commands = {
-            'about_connector': AboutConnector(request_body.args.get('time_zone', default='US/Eastern'))
+            'about_connector': AboutConnector(request_body),
+            'get_bookings': GetBookings(request_body),
         }
         return commands[self.command].response
 
 
 class RoomWizardCommand:
-    def __init__(self, tz):
-        self.tz = tz
-        self.date = arrow.utcnow().to(tz).format('YYYYMMDD')
-        self.time = arrow.utcnow().to(tz).format('HHmmss')
+    def __init__(self, request_body):
+        self.tz = request_body.args.get('time_zone', default='US/Eastern')
+        self.date = arrow.utcnow().to(self.tz).format('YYYYMMDD')
+        self.time = arrow.utcnow().to(self.tz).format('HHmmss')
 
 
 class AboutConnector(RoomWizardCommand):
-    def __init__(self, tz):
-        super().__init__(tz)
+    def __init__(self, request_body):
+        super().__init__(request_body)
         self.connector_name = "Mark's Fabulous Room Wizard Connector"
         self.response = self.__build_response().strip()
 
@@ -42,6 +42,57 @@ class AboutConnector(RoomWizardCommand):
                 <kwe:version>3.1</kwe:version>
                 <kwe:api name="Room Booking API" version="1.0"></kwe:api>
                 <kwe:api name="Room Booking API" version="1.1"></kwe:api>
+            </kwe:connector>
+        </kwe:result>
+        """
+
+
+class GetBookings(RoomWizardCommand):
+    def __init__(self, request_body):
+        super().__init__(request_body)
+        self.room_id = request_body.args.get('room_id', default='LIB_605')
+        self.range_start_date = request_body.args.get('range_start_date', default="today")
+        self.range_start_time = request_body.args.get('range_start_time', default="now")
+        self.range_end_date = request_body.args.get('range_end_date', default=arrow.utcnow().to(self.tz).shift(days=+1).format('YYYYMMDD'))
+        self.range_end_time = request_body.args.get('range_end_time', default=arrow.utcnow().to(self.tz).shift(days=+1).format('HHmmss'))
+        self.response = self.__build_response().strip()
+
+    def __build_response(self):
+        return f"""
+        <?xml version="1.0"?>
+        <kwe:result
+            xmlns:kwe="http://www.appliancestudio.com/kwe/1.0"
+            xmlns:rb="http://www.appliancestudio.com/rb/1.0"
+        >
+            <kwe:date>{self.date}</kwe:date>
+            <kwe:time>{self.time}</kwe:time>
+            <kwe:result_code>0</kwe:result_code>
+            <kwe:connector>
+                <kwe:version>3.1</kwe:version>
+                <kwe:api name="Room Booking API" version="1.0"></kwe:api>
+                <kwe:api name="Room Booking API" version="1.1"></kwe:api>
+                <rb:bookings room_id="{self.room_id}">
+                    <rb:booking booking_id="mark123abc" confidential="no" password_protected="no">
+                        <rb:start_date>
+                            {self.range_start_date}
+                        </rb:start_date>
+                        <rb:end_date>
+                            {self.range_start_date}
+                        </rb:end_date>
+                        <rb:start_time>
+                            220000
+                        </rb:start_time>
+                        <rb:end_time>
+                            230000
+                        </rb:end_time>
+                        <rb:purpose>
+                            Mark's Meeting
+                        </rb:purpose>
+                        <rb:notes>
+                            Mark rules!
+                        </rb:notes>
+                    </rb:booking>
+                </rb:bookings>
             </kwe:connector>
         </kwe:result>
         """
