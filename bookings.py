13568117d00a1fd4952e98795fbfeb03e19df131
Mark Baggett <mbagget1@utk.edu>
2022-07-22 12:19:03 -0500

    Init authentication.

--- bookings.py
+++ bookings.py
@@ -12,5 +12,11 @@ def rw_connector():
     return x.response, 200, {'Content-Type': 'text/xml; charset=utf-8'}
 
 
+@app.route('/admin/sign/RWUnitTypeAction')
+def authentication():
+    if request.method == 'POST':
+        return "Success", 200, {'Content-Type': 'text/xml; charset=utf-8'}
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
