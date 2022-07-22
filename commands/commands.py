b0667ebb51e60fd24b19e32380f957117afa24da
Mark Baggett <mbagget1@utk.edu>
2022-07-22 09:35:36 -0500

    Use timezone information.

--- commands/commands.py
+++ commands/commands.py
@@ -1,9 +1,17 @@
 import arrow
 
 
-class AboutConnector:
+class RoomWizardCommand:
     def __init__(self, tz):
         self.tz = tz
+        self.date = arrow.utcnow().to(tz).format('YYYYMMDD')
+        self.time = arrow.utcnow().to(tz).format('HHmmss')
+
+
+class AboutConnector(RoomWizardCommand):
+    def __init__(self, tz):
+        super().__init__(tz)
+        self.connector_name = "Mark's Fabulous Room Wizard Connector"
         self.response = self.__build_response().strip()
 
     def __build_response(self):
@@ -13,11 +21,11 @@ class AboutConnector:
             xmlns:kwe="http://www.appliancestudio.com/kwe/1.0"
             xmlns:rb="http://www.appliancestudio.com/rb/1.0"
         >
-            <kwe:date>{arrow.utcnow().format('YYYYMMDD')}</kwe:date>
-            <kwe:time>{arrow.utcnow().format('HHmmss')}</kwe:time>
+            <kwe:date>{self.date}</kwe:date>
+            <kwe:time>{self.time}</kwe:time>
             <kwe:result_code>0</kwe:result_code>
             <kwe:connector>
-                <kwe:name>Mark's Fabulous Room Wizard Connector</kwe:name>
+                <kwe:name>{self.connector_name}</kwe:name>
                 <kwe:version>3.1</kwe:version>
                 <kwe:api name="Room Booking API" version="1.0"></kwe:api>
                 <kwe:api name="Room Booking API" version="1.1"></kwe:api>
