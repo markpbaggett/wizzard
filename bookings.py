9b1363859cff16f23ca3ed82cf474902b3c615c7
Mark Baggett <mbagget1@utk.edu>
2022-07-22 12:41:07 -0500

    Allow posts for authentication.

--- bookings.py
+++ bookings.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from commands.commands import GetResponse
+import json
 
 app = Flask(__name__)
 
@@ -12,10 +13,18 @@ def rw_connector():
     return x.response, 200, {'Content-Type': 'text/xml; charset=utf-8'}
 
 
-@app.route('/admin/sign/RWUnitTypeAction')
+@app.route('/admin/sign/RWUnitTypeAction', methods=['POST', 'GET'])
 def authentication():
     if request.method == 'POST':
         print('This is running')
+        response = app.response_class(
+            response=json.dumps({"status": "success", "code": 0, "data": {"Name": "Eyong", "Age": 30}}),
+            status=200,
+            mimetype='application/json'
+        )
+        return response
+    if request.method == 'GET':
+        print('This is a get.')
         return "Success", 200, {'Content-Type': 'text/xml; charset=utf-8'}
 
 
