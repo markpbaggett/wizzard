f2f78f69a2cbb28f37905164f4918645fb53d0f5
Mark Baggett <mbagget1@utk.edu>
2022-07-22 09:24:06 -0500

    Format dates properly.

--- commands/commands.py
+++ commands/commands.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+import arrow
 
 
 class AboutConnector:
     def __init__(self, tz):
         self.tz = tz
-        self.response = self.__build_response()
+        self.response = self.__build_response().strip()
 
     def __build_response(self):
         return f"""
@@ -13,8 +13,8 @@ class AboutConnector:
             xmlns:kwe="http://www.appliancestudio.com/kwe/1.0"
             xmlns:rb="http://www.appliancestudio.com/rb/1.0"
         >
-            <kwe:date>{datetime.now()}</kwe:date>
-            <kwe:time>{datetime.now()}</kwe:time>
+            <kwe:date>{arrow.utcnow().format('YYYYMMDD')}</kwe:date>
+            <kwe:time>{arrow.utcnow().format('HHmmss')}</kwe:time>
             <kwe:result_code>0</kwe:result_code>
             <kwe:connector>
                 <kwe:name>Mark's Fabulous Room Wizard Connector</kwe:name>
@@ -23,4 +23,4 @@ class AboutConnector:
                 <kwe:api name="Room Booking API" version="1.1"></kwe:api>
             </kwe:connector>
         </kwe:result>
-        """.strip()
+        """
