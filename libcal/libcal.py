0748b78a28cf3954f132409f941296e71c3a388e
Mark Baggett <mbagget1@utk.edu>
2022-07-24 10:56:38 -0500

    Configure RoomBookings to work with GenerateToken.

--- libcal/libcal.py
+++ libcal/libcal.py
@@ -1,11 +1,15 @@
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class GenerateToken:
     def __init__(self, id, secret):
         self.endpoint = "https://libcal.utk.edu/1.1/oauth/token"
         self.payload = self.__build_payload(id, secret)
-        self.token = self.__request_token()
+        self.token = {"Authorization": f"Bearer {self.__request_token()}"}
 
     @staticmethod
     def __build_payload(client_id, secret):
@@ -18,3 +22,18 @@ class GenerateToken:
     def __request_token(self):
         r = requests.post(self.endpoint, json=self.payload)
         return r.json()['access_token']
+
+
+class RoomBookings:
+    def __init__(self, group_id, date):
+        self.group_id = group_id
+        self.date = date
+        self.endpoint = f"https://libcal.utk.edu/1.1/room_bookings_nickname?group_id={group_id}&date={date}"
+        self.headers = {"Content-Type": "application/json; charset=utf-8"}
+
+    def get_bookings(self):
+        headers = GenerateToken(os.getenv('id'), os.getenv('secret')).token
+        r = requests.get(self.endpoint, headers=headers)
+        return r.json()
+
+
