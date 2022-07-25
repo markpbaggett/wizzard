058d9b8a6e82a8901b5a6a054c95c11abcb90626
Mark Baggett <mbagget1@utk.edu>
2022-07-25 17:39:42 -0400

    Document all routes.

--- bookings.py
+++ bookings.py
@@ -9,21 +9,39 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/rwconnector')
 def rw_connector():
+    """Routes GET requests to root or /rwconnector.
+
+    This route handles all traffic to / or /rwconnector. The route passes all HTTP parameters to the GetResponse in
+    the commands package. Because of that, both about_connector and get_bookings routing is handled here.
+
+    Returns:
+        tuple: A tuple with the appropriate XML response from the related API at index 0, 200 at index 1, and a dict
+        specifying the content type of the response at index 2.
+
+    """
     x = GetResponse(request)
     return x.response, 200, {'Content-Type': 'text/xml; charset=utf-8'}
 
 
-@app.route('/admin/sign/RWUnitTypeAction', methods=['POST', 'GET'])
+@app.route('/admin/sign/RWUnitTypeAction', methods=['POST'])
 def authentication():
-    if request.method == 'POST':
-        response = app.response_class(
-            response=json.dumps({"status": "success", "code": 0, "data": {"Name": "Eyong", "Age": 30}}),
-            status=200,
-            mimetype='application/json'
-        )
-        return response
-    if request.method == 'GET':
-        return "Success", 200, {'Content-Type': 'text/xml; charset=utf-8'}
+    """Routes POST requests to the expected authentication endpoint for validation.
+
+        This route handles all POST traffic to /admin/sign/RWUnitTypeAction. Since we do not use authentication, the
+        response is always a Flask Response with a JSON success message, a status code of 200, and a mimetype of
+        'application/json'.
+
+        Returns:
+            flask.wrappers.Response: a Flask Response with a JSON success message, a status code of 200, and a mimetype
+            of 'application/json'
+
+        """
+    response = app.response_class(
+        response=json.dumps({"status": "success", "code": 0, "data": {"Success": True}}),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
 
 
 if __name__ == '__main__':
